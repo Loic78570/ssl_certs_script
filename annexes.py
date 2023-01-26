70eee4ed3e00006ab8aa8adb35f9b2582eb1d489
Loïc GAVENS <loic.2010@live.fr>
2023-01-27 00:19:52 +0100

    cleaning code to have functions generating certs and csr

---
+++ annexes.py
@@ -0,0 +1,86 @@
+import datetime
+
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+def generate_private_key():
+    """
+    Cette fonction génère une clée privée.
+
+    :param encoding: Nom de l'encodage
+
+    :param keyname: Nom de la clé à enregistrer
+
+    :param passphrase: Mot de passe de la clé
+
+    :return:
+    """
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    return private_key
+
+
+def generate_cert(subject_cert: x509.Name, issuer_cert: x509.Name, key_to_sign: rsa.RSAPrivateKey,
+                  public_key: rsa.RSAPublicKey):
+    cert = x509.CertificateBuilder().subject_name(
+
+        subject_cert
+    ).issuer_name(
+
+        issuer_cert
+    ).public_key(
+
+        public_key
+    ).serial_number(
+
+        x509.random_serial_number()
+    ).not_valid_before(
+
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+        datetime.datetime.utcnow() + datetime.timedelta(days=1000)
+    ).add_extension(
+
+        x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
+        critical=False,
+        # Sign our certificate with our private key
+    ).sign(key_to_sign, hashes.SHA256())
+
+    return cert
+
+
+def generate_CA(subject_cert, issuer_cert):
+    key = generate_private_key()
+    # Write our key to disk for safe keeping
+    cert = generate_cert(subject_cert=subject_cert, issuer_cert=issuer_cert, key_to_sign=key, public_key=key.public_key())
+
+    # Various details about who we are. For a self-signed certificate the
+    # subject and issuer are always the same.
+
+    # Write our certificate out to disk.
+
+    return cert, key
+
+
+def generate_csr(subject_cert: x509.Name, issuer_cert: x509.Name,):
+    key_priv = generate_private_key()
+
+    # Generate a CSR
+    csr = x509.CertificateSigningRequestBuilder().subject_name(subject_cert).add_extension(
+        x509.SubjectAlternativeName([
+            # Describe what sites we want this certificate for.
+            x509.DNSName(u"mysite.com"),
+            x509.DNSName(u"www.mysite.com"),
+            x509.DNSName(u"subdomain.mysite.com"),
+        ]),
+        critical=False,
+        # Sign the CSR with our private key.
+    ).sign(key_priv, hashes.SHA256())
+
+    return csr, key_priv
+
+def sign_csr(csr_cert: x509.CertificateSigningRequest, issuername: x509.Name, key_to_sign: rsa.RSAPrivateKey):
+    return generate_cert(subject_cert=csr_cert.subject, issuer_cert=issuername, key_to_sign=key_to_sign, public_key=key_to_sign.public_key())
--- mainv2.py
+++ mainv2.py
@@ -1,49 +1,20 @@
-import asyncio
-import datetime
-import types
-
 import colorama
 import cryptography.x509
 from colorama import Fore
 from cryptography import x509
-from cryptography.hazmat._oid import NameOID
-from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.exceptions import InvalidSignature
+from cryptography.x509.base import serialization
+from cryptography.x509.oid import NameOID
 
-
-def generate_private_key(keyname: str, passphrase: bytes, encoding: serialization.Encoding):
-    """
-    Cette fonction génère une clée privée.
-
-    :param encoding: Nom de l'encodage
-
-    :param keyname: Nom de la clé à enregistrer
-
-    :param passphrase: Mot de passe de la clé
-
-    :return:
-    """
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-    )
-    with open(keyname, "wb") as file:
-        file.write(private_key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.BestAvailableEncryption(passphrase),
-        ))
-    return private_key
+from annexes import *
 
 
 if __name__ == "__main__":
+    print("Génération du CA root...", end="")
     # Generate our key
-    key = generate_private_key("key.pem", b"passphrase", serialization.Encoding.PEM)
-    # Write our key to disk for safe keeping
 
-    # Various details about who we are. For a self-signed certificate the
-    # subject and issuer are always the same.
     subject = issuer = x509.Name([
         x509.NameAttribute(NameOID.COUNTRY_NAME, u"FR"),
         x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"Val d'Oise"),
@@ -51,61 +22,50 @@ if __name__ == "__main__":
         x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"CY TECH"),
         x509.NameAttribute(NameOID.COMMON_NAME, u"cy-tech.fr"),
     ])
-    cert = x509.CertificateBuilder().subject_name(
 
-        subject
-    ).issuer_name(
+    certificate, priv_key = generate_CA(subject, issuer)
 
-        issuer
-    ).public_key(
-
-        key.public_key()
-    ).serial_number(
-
-        x509.random_serial_number()
-    ).not_valid_before(
-
-        datetime.datetime.utcnow()
-    ).not_valid_after(
-        datetime.datetime.utcnow() + datetime.timedelta(days=1000)
-    ).add_extension(
-
-        x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
-        critical=False,
-        # Sign our certificate with our private key
-    ).sign(key, hashes.SHA256())
-    # Write our certificate out to disk.
     with open("certificate.pem", "wb") as pub_key:
-        pub_key.write(cert.public_bytes(serialization.Encoding.PEM))
+        pub_key.write(certificate.public_bytes(serialization.Encoding.PEM))
+
+    with open("key.pem", "wb") as file:
+        file.write(priv_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.BestAvailableEncryption(b"passphrase"),
+        ))
+
+    print(Fore.LIGHTGREEN_EX, "Terminé!", Fore.RESET, end=None)
 
     ## CSR
 
-    # Generate our key
-    key = generate_private_key("sub_key.pem", b"passphrase", serialization.Encoding.PEM)
 
-    # Generate a CSR
-    csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
+
+    print("Génération du CA intermédiaire...", end="")
+    # Generate our key
+
+    subject = x509.Name([
         # Provide various details about who we are.
         x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
         x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
         x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
         x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
         x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com"),
-    ])).add_extension(
-        x509.SubjectAlternativeName([
-            # Describe what sites we want this certificate for.
-            x509.DNSName(u"mysite.com"),
-            x509.DNSName(u"www.mysite.com"),
-            x509.DNSName(u"subdomain.mysite.com"),
-        ]),
-        critical=False,
-        # Sign the CSR with our private key.
-    ).sign(key, hashes.SHA256())
+    ])
+
+    csr, priv_key_csr = generate_csr(subject_cert=subject, issuer_cert=subject)
+
     # Write our CSR out to disk.
     with open("sub_csr.pem", "wb") as pub_key:
         pub_key.write(csr.public_bytes(serialization.Encoding.PEM))
 
+    print(Fore.LIGHTGREEN_EX, "Terminé!", Fore.RESET, end=None)
     ## signature du certificat CSR par le root
+
+
+    # signing csr
+
+    print("Signature du sub CA par le CA Root...", end="")
 
     cert_root = open("certificate.pem", 'rb').read()
     cert_root_pkey = open("key.pem", 'rb').read()
@@ -118,10 +78,6 @@ if __name__ == "__main__":
     csr = cryptography.x509.load_pem_x509_csr(bytes(csr_sub))
     root = cryptography.x509.load_pem_x509_certificate(bytes(cert_root))
 
-    # signing csr
-
-    key = csr_sub_pkey
-
     # Various details about who we are. For a self-signed certificate the
     # subject and issuer are always the same.
     issuer = x509.Name([
@@ -133,42 +89,23 @@ if __name__ == "__main__":
                            root.issuer.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value),
         x509.NameAttribute(NameOID.COMMON_NAME, root.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value),
     ])
+
     subject = csr.subject
-    cert = x509.CertificateBuilder().subject_name(
 
-        subject
-    ).issuer_name(
+    cert = sign_csr(csr_cert=csr, issuername=issuer, key_to_sign=cert_root_pkey)
 
-        issuer
-    ).public_key(
-
-        key.public_key()
-    ).serial_number(
-
-        x509.random_serial_number()
-    ).not_valid_before(
-
-        datetime.datetime.utcnow()
-    ).not_valid_after(
-        datetime.datetime.utcnow() + datetime.timedelta(days=1000)
-    ).add_extension(
-
-        x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
-        critical=False,
-        # Sign our certificate with our private key ?
-    ).sign(cert_root_pkey, hashes.SHA256())
     # Write our certificate out to disk.
     with open("certificate_signed.pem", "wb") as pub_key:
         pub_key.write(cert.public_bytes(serialization.Encoding.PEM))
+    print(Fore.LIGHTGREEN_EX, "Terminé!", Fore.RESET, end=None)
 
-    print(cert.public_key().public_bytes(
-        cryptography.x509.base.serialization.Encoding.PEM,
-        cryptography.x509.base.serialization.PublicFormat.PKCS1
-    ))
-    print(colorama.Fore.LIGHTCYAN_EX)
-    print("SUJET SUJET", cert.subject)
-    print("SUJET ROOT ", cert.issuer)
-    print(colorama.Fore.RESET)
+    # print(cert.public_key().public_bytes(
+    #     cryptography.x509.base.serialization.Encoding.PEM,
+    #     cryptography.x509.base.serialization.PublicFormat.PKCS1
+    # ))
+
+    print("SUJET ROOT ", colorama.Fore.LIGHTCYAN_EX, root.issuer, colorama.Fore.RESET)
+    print("SUJET SUJET", colorama.Fore.LIGHTCYAN_EX, cert.subject, colorama.Fore.RESET)
 
     # verification :
     print("Verification de la hiérarchie du certificat... ", end="")
